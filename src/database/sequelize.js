be91960a24983a800cfa9765b265990777f6b573
Your Name <you@example.com>
2024-08-08 19:40:33 +0300

    create default root user

--- src/database/sequelize.js
+++ src/database/sequelize.js
@@ -4,8 +4,8 @@ const taskModel = require('../model/taskModel');
 const subTaskModel = require('../model/subTaskModel');
 const notificationModel = require('../model/notificationModel');
 
-const sequelize = new Sequelize('aquatic-service', 'postgres', '1212', {
-    host: 'localhost',
+const sequelize = new Sequelize( process.env.DATABASE_NAME, process.env.DATABASE_USER_NAME, process.env.DATABASE_USER_PASSWORD, {
+    host: process.env.DATABASE_HOST,
     dialect: 'postgres',
     define: {
         freezeTableName: true
@@ -31,13 +31,22 @@ const initDb = async _=> {
     User.hasMany( Notification, { foreignKey: 'notification_receiver', allowNull: false })
     Notification.belongsTo( User, {foreignKey: 'notification_receiver', onDelete: 'CASCAD'})
 
-    sequelize.sync()
+    await sequelize.sync()
+    .then( async _=>{
+        await User.findOrCreate({
+            where: { user_email: 'root@gmail.com'},
+            defaults: {
+                user_email: 'root@gmail.com',
+                user_name: 'root',
+                user_password: 'root',
+                user_role: 'approving',
+                user_account_status: 'enabled'
+            }
+        });
+    })
+    .catch( error => console.log(error))
     // sequelize.sync({ alter: true })
     // sequelize.sync({ force: true })
-
-    await User.findOrCreate({
-        where: { user_name: 'root', user_email: 'root@gmail.com', user_password: 'root', user_role: 'approving', user_account_status: 'enabled'}
-    });
 }
 
 
